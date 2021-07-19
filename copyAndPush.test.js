329ab09d3d1351f83c6426bf28a72eb7fd219a84
DJ Drakos <dianajodrakos@gmail.com>
2021-07-19 16:48:35 -0700

    added additional testing to confirm original array has not been mutated

--- copyAndPush.test.js
+++ copyAndPush.test.js
@@ -1,12 +1,21 @@
 import { copyAndPush } from './copyAndPush';
 
+const numbers = [1, 2, 3];
 
 describe('copyAndPush', () => {
-  it('returns a copy of the array with new item pushed to the end', () => {
-    const numbers = [1, 2, 3];
+  it('returns a copy of the array with 4 pushed to the end', () => {
     const expected = [1, 2, 3, 4];
     const actual = copyAndPush(numbers, 4);
 
     expect(actual).toEqual(expected);
+    expect(numbers).not.toEqual(expected);
+  });
+
+  it('returns a copy of the array with 5 pushed to the end', () => {
+    const expected = [1, 2, 3, 5];
+    const actual = copyAndPush(numbers, 5);
+
+    expect(actual).toEqual(expected);
+    expect(numbers).not.toEqual(expected);
   });
 });
