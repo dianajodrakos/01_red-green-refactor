f5390b3ffc1e05aa434d052728de0fa821129b71
DJ Drakos <dianajodrakos@gmail.com>
2021-07-19 17:19:51 -0700

    refactored function to use f.filter and .map methods instead of a for loop

--- capitalizeAndFilter.js
+++ capitalizeAndFilter.js
@@ -1,10 +1,7 @@
 
 export function capitalizeAndFilter(array) {
-  const newArray = [];
-  for (const item of array) {
-    if (item.charAt(0) !== 'f' && item.charAt(0) !== 'F') {
-      newArray.push(item.toUpperCase());
-    }
-  }
-  return newArray;
+  return array
+    .filter(item => item.charAt(0) !== 'f' && item.charAt(0) !== 'F')
+    .map(item => item.toUpperCase()
+    );
 }
